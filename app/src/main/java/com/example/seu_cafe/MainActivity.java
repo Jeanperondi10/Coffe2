ca8e32f58177ba97e9b4f10c44d1d669b1d5d474
Jean Perondi <jeanperondi10@gmail.com>
2019-06-10 14:52:36 -0300

    Atualizacão com email
    Signed-off-by: Jean Perondi <jeanperondi10@gmail.com>

--- app/src/main/java/com/example/seu_cafe/MainActivity.java
+++ app/src/main/java/com/example/seu_cafe/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.seu_cafe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     int n = 0, preco = 0, total = 0;
@@ -45,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
     public void button6(View view) {
         preco = 4;
         calculo();
-    }//DFSADA
+    }
 
     public void reduz(View view) {
         if (n > 0) {
@@ -67,6 +69,32 @@ public class MainActivity extends AppCompatActivity {
         totalView.setText("Preco Total: " + total);
     }
 
+    public void toast(View view) {
+        Toast msg;
+        if (n > 1) {
+            msg = Toast.makeText(getApplicationContext(), "Voce comprou " + n + " cafes!", Toast.LENGTH_LONG);
+        } else {
+            msg = Toast.makeText(getApplicationContext(), "Voce comprou " + n + " cafe!", Toast.LENGTH_LONG);
+        }
+        msg.show();
+
+        /*
+        Button btn=findViewById(R.id.botao);
+
+        btn.set
+
+
+
+        */
+
+        Intent sendEmail = new Intent(Intent.ACTION_SEND);
+        sendEmail.putExtra(Intent.EXTRA_EMAIL, "jeanperondi10@gmail.com");
+        sendEmail.putExtra(Intent.EXTRA_TEXT, "Olá Jean Perondi sou seu app");
+        sendEmail.putExtra(Intent.EXTRA_SUBJECT, "Perondi");
+        if (sendEmail.resolveActivity(getPackageManager()) != null) {
+            startActivity(sendEmail);
+        }
+    }
 
 
 }
